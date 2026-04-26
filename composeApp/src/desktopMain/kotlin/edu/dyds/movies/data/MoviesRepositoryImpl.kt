6d874a190dce23635ebb00513dfa4ad0026ad2aa
cico <nicocidips@gmail.com>
2026-04-26 05:34:17 -0300

    Refactor(data y domain): mover logica de negocio presente en MoviesRepository a UseCase. [GRAVE - G6]

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/MoviesRepositoryImpl.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/MoviesRepositoryImpl.kt
@@ -3,37 +3,22 @@ package edu.dyds.movies.data
 import edu.dyds.movies.data.external.RemoteMoviesDataSource
 import edu.dyds.movies.data.local.LocalMoviesCache
 import edu.dyds.movies.domain.entity.Movie
-import edu.dyds.movies.domain.entity.QualifiedMovie
 import edu.dyds.movies.domain.repository.MoviesRepository
-
-private const val MIN_VOTE_AVERAGE = 6.0
 
 class MoviesRepositoryImpl(
     private val remoteMoviesDataSource: RemoteMoviesDataSource,
     private val localMoviesCache: LocalMoviesCache
 ) : MoviesRepository {
 
-    override suspend fun getPopularMovies(): List<QualifiedMovie> {
-        val movies = localMoviesCache.getCachedMovies() ?: run {
+    override suspend fun getPopularMovies(): List<Movie> {
+        return localMoviesCache.getCachedMovies() ?: run {
             val fetched = remoteMoviesDataSource.getPopularMovies().map { it.toDomainMovie() }
             localMoviesCache.saveMovies(fetched)
             fetched
         }
-        return movies.sortAndMap()
     }
 
     override suspend fun getMovieDetails(id: Int): Movie? {
         return remoteMoviesDataSource.getMovieDetails(id)?.toDomainMovie()
-    }
-
-    private fun List<Movie>.sortAndMap(): List<QualifiedMovie> {
-        return this
-            .sortedByDescending { it.voteAverage }
-            .map {
-                QualifiedMovie(
-                    movie = it,
-                    isGoodMovie = it.voteAverage >= MIN_VOTE_AVERAGE
-                )
-            }
     }
 }
--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/repository/MoviesRepository.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/repository/MoviesRepository.kt
@@ -1,9 +1,8 @@
 package edu.dyds.movies.domain.repository
 
 import edu.dyds.movies.domain.entity.Movie
-import edu.dyds.movies.domain.entity.QualifiedMovie
 
 interface MoviesRepository {
-    suspend fun getPopularMovies(): List<QualifiedMovie>
+    suspend fun getPopularMovies(): List<Movie>
     suspend fun getMovieDetails(id: Int): Movie?
 }
